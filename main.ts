d7a40b9530663445ce5877cd7c9ecc050c4b3e54
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-05-14 22:42:11 +0400

    Solution, implementing API's

---
+++ main.ts
@@ -0,0 +1,31 @@
+// Global variables for keeping x_n+1 x_n
+let fib1 = BigInt(1)
+let fib2 = BigInt(1)
+let fib3 = BigInt(0)
+
+
+
+
+function fibonacci(n) {
+    if (n == 1) {
+        console.log(1)
+        return
+    }
+    else if (n == 2) {
+        console.log(1 + ", " + 1)
+        return
+    }
+    for (n -2; n>0;) {
+
+        console.log(fib1 + ", ")
+        fib1 = fib2+fib3
+        fib3 = fib2
+        fib2 = fib1
+
+        n--
+    }
+
+}
+
+
+fibonacci(10)
